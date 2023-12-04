0d6708c9bd3b08e75f8958eb04c5c437f4ca28b5
Vaibhav Singh <72432697+Vaibhav-Singh21@users.noreply.github.com>
2023-12-04 18:15:33 +0530

    Solved Anagram

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,6 +5,28 @@
 */
 
 function isAnagram(str1, str2) {
+  let STR1 = str1.toLowerCase().split('').sort().join('');
+  let STR2 = str2.toLowerCase().split('').sort().join('');
+
+  // let strArr1 = [...STR1];
+  // let strArr2 = [...STR2];
+
+  // if strings are of different length that can not be anagram.
+  if (STR1.length != STR2.length) {
+    return false;
+  }
+
+  //sorting it
+  // strArr1.sort();
+  // strArr2.sort();
+
+  // for (let index = 0; index < len; index++) {
+  //   if (strArr1[index] != strArr2[index])
+  //     return false;
+
+  // }
+
+  return STR1 === STR2;
 
 }
 
